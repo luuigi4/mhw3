c6b6302d5f4faa9f73910a34f8e66e2b59403084
luuigi4 <81307951+luuigi4@users.noreply.github.com>
2021-04-24 11:42:37 +0200

    Add files via upload

---
+++ content.js
@@ -0,0 +1,102 @@
+const contenuto =[
+{
+	titolo:'Hangover',
+	immagine:'hangover.jpg',
+	descrizione:"Film cult della cultura pop americana, viene trasmesso in Italia da Netflix",	
+},
+
+{
+	titolo:'Joker',
+	immagine:'Joker.jpg',
+	descrizione:"Il film con la magistrale interpretazione di Joaquin Phoenix, e' su Infinity",	
+},
+
+{
+	titolo:'The Avengers',
+	immagine:'avengers.jpg',
+	descrizione:"Gli Avengers collaborano tutti per sconfiggere Loki.Che aspetti? Va' su Disney+",	
+},
+
+{
+	titolo:'Bohemian',
+	immagine:'bohemian.jpg',
+	descrizione:"Il documentario sui Queen e sul loro frontman Freddie Mercury e' su Prime Video",	
+},
+
+{
+	titolo:'Il re leone',
+	immagine:'re_leone.jpg',
+	descrizione:"Il remake del 2019 di uno dei piu' famosi classici per bambini si trova su Disney+",	
+},
+
+{
+	titolo:'Game of Thrones',
+	immagine:'game_of_thrones.jpg',
+	descrizione:"La serie tv fantasy più famosa del mondo e' trasmessa in Italia in esclusica su Now",	
+},
+
+{
+	titolo:'Breaking Bad',
+	immagine:'brba.jpg',
+	descrizione:"La serie sul buon padre di famiglia Walter White e' trasmessa in Italia da Netflix",	
+},
+
+{
+	titolo:'Tenet',
+	immagine:'tenet.jpg',
+	descrizione:"Il thriller del tempo di Christopher Nolan e' trasmesso in esclusiva su Infinity",	
+},
+
+{
+	titolo:'V per Vendetta',
+	immagine:'v-per-vendetta.jpg',
+	descrizione:'Il film cult tratto dal romanzo a fumetti per cui Netflix ha acquistato i diritti',	
+},
+
+{
+	titolo:'Justice League',
+	immagine:'justice-league.jpg',
+	descrizione:'Gli eroi uniti della Dc Comics diretto da Zack Snyder sono in esclusiva su Now',	
+},
+
+{
+	titolo:'Mr Robot',
+	immagine:'mr_robot.jpg',
+	descrizione:"La serie dell'hacker migliore del mondo e' trasmesso in esclusiva su Prime Video ",	
+},
+
+{
+	titolo:'Pirati Dei Caraibi',
+	immagine:'pdc.jpg',
+	descrizione:'Jack Sparrow e la sua ciurma di pirati sono approdati in esclusiva su Disney+',	
+},
+
+{
+	titolo:	'Gemini Man',
+	immagine:'gemini.jpg',
+	descrizione:'Will Smith e il suo clone in un universo distopico sono trasmessi su  Prime video',	
+},
+
+{
+	titolo:'Casa di carta',
+	immagine:'papel.jpg',
+	descrizione:'La serie tv spagnola che ha avuto fama in tutto il mondo sono esclusiva targata Netflix',	
+},
+
+{
+	titolo:'Birds of Prey',
+	immagine:'prey.jpg',
+	descrizione:"Lo spinoff riguardante Harley Quinn e'	trasmesso in Italia in esclusiva da Now",	
+},
+
+{
+	titolo:'Signore degli anelli',
+	immagine:'lotr.jpg',
+	descrizione:"La trilogia fantasy basata sul libro di Tolkien e' trasmessa in Italia da infinity",	
+},
+]
+
+
+
+
+
